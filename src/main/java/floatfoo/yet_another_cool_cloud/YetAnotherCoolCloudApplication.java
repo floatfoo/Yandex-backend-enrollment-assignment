e49421b2c50fa2c3fa0782ab7ef6cded996bd78e
floatfoo <floatfoo@gmail.com>
2022-09-12 11:44:06 +0300

    init commit

---
+++ src/main/java/floatfoo/yet_another_cool_cloud/YetAnotherCoolCloudApplication.java
@@ -0,0 +1,13 @@
+package floatfoo.yet_another_cool_cloud;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class YetAnotherCoolCloudApplication {
+
+    public static void main(String[] args) {
+        SpringApplication.run(YetAnotherCoolCloudApplication.class, args);
+    }
+
+}
---
+++ src/test/java/floatfoo/yet_another_cool_cloud/YetAnotherCoolCloudApplicationTests.java
@@ -0,0 +1,13 @@
+package floatfoo.yet_another_cool_cloud;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class YetAnotherCoolCloudApplicationTests {
+
+    @Test
+    void contextLoads() {
+    }
+
+}
