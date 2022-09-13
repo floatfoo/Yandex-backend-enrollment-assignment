77f15680c7c25e7ce90d62b2be6c0f8bf864e17d
floatfoo <floatfoo@gmail.com>
2022-09-13 15:34:02 +0300

    File && Folder entities

---
+++ src/main/java/floatfoo/yet_another_cool_cloud/entity/File.java
@@ -0,0 +1,92 @@
+package floatfoo.yet_another_cool_cloud.entity;
+
+import org.hibernate.annotations.Type;
+import org.springframework.lang.NonNull;
+
+import javax.persistence.*;
+import java.time.OffsetDateTime;
+import java.util.Objects;
+
+@Entity
+@Table(name = "file")
+public class File extends SystemItem {
+    @Id
+    @Column(name = "id", nullable = false)
+    private String id;
+
+    @Column(name = "date")
+    private OffsetDateTime date;
+
+    @ManyToOne(fetch = FetchType.LAZY)
+    @JoinColumn(name = "parent_id")
+    private Folder parent;
+
+    @Column(name = "size")
+    private Integer size;
+    @Column(name = "url")
+    @Type(type = "org.hibernate.type.TextType")
+    private String url;
+
+    public String getId() {
+        return id;
+    }
+
+    public void setId(String id) {
+        this.id = id;
+    }
+
+    public OffsetDateTime getDate() {
+        return date;
+    }
+
+    public void setDate(OffsetDateTime date) {
+        this.date = date;
+    }
+
+    public Folder getParent() {
+        return parent;
+    }
+
+    public void setParent(Folder parent) {
+        this.parent = parent;
+    }
+
+    public Integer getSize() {
+        return size;
+    }
+
+    public void setSize(Integer size) {
+        this.size = size;
+    }
+
+    public String getUrl() {
+        return url;
+    }
+
+    public void setUrl(String url) {
+        this.url = url;
+    }
+
+    @Override
+    public String toString() {
+        return "File{" +
+                "id='" + id + '\'' +
+                ", date=" + date +
+                ", parent=" + parent +
+                ", size=" + size +
+                ", url='" + url + '\'' +
+                '}';
+    }
+
+    @Override
+    public boolean equals(Object o) {
+        if (this == o) return true;
+        if (!(o instanceof File file)) return false;
+        return id.equals(file.id);
+    }
+
+    @Override
+    public int hashCode() {
+        return Objects.hash(id);
+    }
+}
