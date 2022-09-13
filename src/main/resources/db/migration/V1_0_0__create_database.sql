c7adecb610a2b0fe1c9c149b45872faadc75fff7
floatfoo <floatfoo@gmail.com>
2022-09-13 14:51:51 +0300

    First Migration added

---
+++ src/main/resources/db/migration/V1_0_0__create_database.sql
@@ -0,0 +1,28 @@
+create type system_item_type as enum (
+    'file',
+    'folder'
+    );
+
+create table system_item (
+    date timestamptz default current_timestamp,
+    type system_item_type not null
+    );
+
+create table folder (
+    id varchar(255) primary key,
+    parent_id varchar(255) references folder(id)
+) inherits (system_item);
+
+create table file (
+    id varchar(255) primary key,
+    parent_id varchar(255) references folder(id),
+    size integer,
+    url text
+) inherits (system_item);
+
+create table folder_children (
+    parent_folder varchar(255) references folder(id),
+    child_folder varchar(255) references folder(id),
+    child_file varchar(255) references file(id),
+    PRIMARY KEY (parent_folder, child_folder, child_file)
+);
