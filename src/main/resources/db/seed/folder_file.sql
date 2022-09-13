050811118ea4f07379c4506f308ec026026cc289
floatfoo <floatfoo@gmail.com>
2022-09-13 14:54:01 +0300

    Seed data: folder && files

---
+++ src/main/resources/db/seed/folder_file.sql
@@ -0,0 +1,20 @@
+INSERT INTO folder(id, type)
+VALUES ('DIR_1', 'folder');
+
+INSERT INTO folder(id, type, parent_id)
+VALUES ('DIR_2', 'folder', 'DIR_1');
+
+INSERT INTO folder(id, type, parent_id)
+VALUES ('DIR_3', 'folder', 'DIR_1');
+
+INSERT INTO file(id, type, url, parent_id)
+VALUES ('FILE_1', 'file', '~/Documents/Projects', 'DIR_1');
+
+INSERT INTO file(id, type, url, parent_id)
+VALUES ('FILE_2', 'file', '~/Documents/Projects', 'DIR_2');
+
+INSERT INTO file(id, type, url, parent_id)
+VALUES ('FILE_3', 'file', '~/Documents/Projects', 'DIR_3');
+
+INSERT INTO folder_children(parent_folder, child_folder, child_file)
+VALUES ('DIR_1', 'DIR_2', 'DIR_3');
