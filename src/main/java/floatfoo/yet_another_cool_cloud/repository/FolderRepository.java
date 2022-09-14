0dbe5e66b0efb5db2a4dae374aa8520abe6e4fb3
floatfoo <floatfoo@gmail.com>
2022-09-14 16:32:27 +0300

    Add query: search all subfolders and children files of given folder

--- src/main/java/floatfoo/yet_another_cool_cloud/repository/FolderRepository.java
+++ src/main/java/floatfoo/yet_another_cool_cloud/repository/FolderRepository.java
@@ -2,6 +2,7 @@ package floatfoo.yet_another_cool_cloud.repository;
 
 import floatfoo.yet_another_cool_cloud.entity.Folder;
 import floatfoo.yet_another_cool_cloud.entity.SystemItem;
+import org.springframework.data.jpa.repository.Query;
 import org.springframework.data.repository.CrudRepository;
 import org.springframework.stereotype.Repository;
 
@@ -9,4 +10,6 @@ import java.util.List;
 
 @Repository
 public interface FolderRepository extends CrudRepository<Folder, String> {
+    @Query("select f from Folder f where f.parent.id = ?1")
+    List<Folder> findByParent_IdEquals(String id);
 }
