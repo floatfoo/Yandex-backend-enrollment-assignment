f88a830b41c85cc1a30cc9e23183d384aaf656d7
floatfoo <floatfoo@gmail.com>
2022-09-14 13:19:16 +0300

    Seed refactor

--- src/main/resources/db/seed/folder_file.sql
+++ src/main/resources/db/seed/folder_file.sql
@@ -1,20 +1,20 @@
-INSERT INTO folder(id, type)
-VALUES ('DIR_1', 'folder');
+INSERT INTO folder(id)
+VALUES ('DIR_1');
 
-INSERT INTO folder(id, type, parent_id)
-VALUES ('DIR_2', 'folder', 'DIR_1');
+INSERT INTO folder(id, parent_id)
+VALUES ('DIR_2', 'DIR_1');
 
-INSERT INTO folder(id, type, parent_id)
-VALUES ('DIR_3', 'folder', 'DIR_1');
+INSERT INTO folder(id, parent_id)
+VALUES ('DIR_3', 'DIR_1');
 
-INSERT INTO file(id, type, url, parent_id)
-VALUES ('FILE_1', 'file', '~/Documents/Projects', 'DIR_1');
+INSERT INTO file(id, url, parent_id)
+VALUES ('FILE_1', '~/Documents/Projects', 'DIR_1');
 
-INSERT INTO file(id, type, url, parent_id)
-VALUES ('FILE_2', 'file', '~/Documents/Projects', 'DIR_2');
+INSERT INTO file(id, url, parent_id)
+VALUES ('FILE_2', '~/Documents/Projects', 'DIR_2');
 
-INSERT INTO file(id, type, url, parent_id)
-VALUES ('FILE_3', 'file', '~/Documents/Projects', 'DIR_3');
+INSERT INTO file(id, url, parent_id)
+VALUES ('FILE_3', '~/Documents/Projects', 'DIR_3');
 
 INSERT INTO folder_children(parent_folder, child_folder, child_file)
 VALUES ('DIR_1', 'DIR_2', 'FILE_1');
