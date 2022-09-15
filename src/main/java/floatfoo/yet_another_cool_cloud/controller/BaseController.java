2e5c72ed891c4f3eccf42860782f273879e650d5
floatfoo <floatfoo@gmail.com>
2022-09-15 18:05:41 +0300

    unused imports deleted

--- src/main/java/floatfoo/yet_another_cool_cloud/controller/BaseController.java
+++ src/main/java/floatfoo/yet_another_cool_cloud/controller/BaseController.java
@@ -10,9 +10,7 @@ import org.springframework.http.ResponseEntity;
 import org.springframework.lang.NonNull;
 import org.springframework.web.bind.annotation.*;
 
-import javax.websocket.server.PathParam;
 import java.time.Instant;
-import java.util.List;
 
 @RestController
 public class BaseController {
